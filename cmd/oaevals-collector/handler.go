d4ee361570e5911878e8201952ded9824cdd0372
nstankov-bg <me@nstankov.com>
2023-07-21 16:48:46 -0400

    multi-eval

--- cmd/oaevals-collector/handler.go
+++ cmd/oaevals-collector/handler.go
@@ -10,7 +10,7 @@ import (
 	"github.com/nstankov-bg/oaievals-collector/pkg/influxdb"
 	"github.com/nstankov-bg/oaievals-collector/pkg/kafka"
 	"github.com/nstankov-bg/oaievals-collector/pkg/loki"
-	"github.com/nstankov-bg/oaievals-collector/pkg/mongodb" // Newly added import
+	"github.com/nstankov-bg/oaievals-collector/pkg/mongodb"
 	"github.com/nstankov-bg/oaievals-collector/pkg/monitoring"
 	"github.com/nstankov-bg/oaievals-collector/pkg/timescaledb"
 )
@@ -24,77 +24,78 @@ func eventHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var event events.Event
-	err := json.NewDecoder(r.Body).Decode(&event)
+	var eventsList []events.Event
+	err := json.NewDecoder(r.Body).Decode(&eventsList)
 	if err != nil {
 		http.Error(w, "Error unmarshalling JSON: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if event.Type == "match" {
-		// Accessing the value "correct" from the map.
-		correctVal, exists := event.Data["correct"]
-		if exists {
-			correct, ok := correctVal.(bool)
-			if ok {
-				if correct {
-					mon.EventCounter.WithLabelValues(event.RunID, "true").Inc()
+	for _, event := range eventsList {
+		if event.Type == "match" {
+			correctVal, exists := event.Data["correct"]
+			if exists {
+				correct, ok := correctVal.(bool)
+				if ok {
+					if correct {
+						mon.EventCounter.WithLabelValues(event.RunID, "true").Inc()
+					} else {
+						mon.EventCounter.WithLabelValues(event.RunID, "false").Inc()
+					}
 				} else {
-					mon.EventCounter.WithLabelValues(event.RunID, "false").Inc()
+					log.Println("The 'correct' value is not a boolean")
 				}
 			} else {
-				log.Println("The 'correct' value is not a boolean")
+				log.Println("The 'correct' key does not exist in the data")
 			}
-		} else {
-			log.Println("The 'correct' key does not exist in the data")
-		}
 
-		// Check if the environmental variable for InfluxDB is set.
-		influxdbHost := os.Getenv("INFLUXDB_HOST")
-		if influxdbHost != "" {
-			// If set, write to InfluxDB.
-			influxdb.WriteToInfluxDB(event)
-		}
-
-		// Check if the environmental variable for Loki is set.
-		lokiURL := os.Getenv("LOKI_URL")
-		if lokiURL != "" {
-			// If set, write to Loki.
-			loki.WriteToLoki(event)
-		}
-
-		// Check if the environmental variable for TimescaleDB is set.
-		timescaleDBHost := os.Getenv("TIMESCALEDB_HOST")
-		if timescaleDBHost != "" {
-			// If set, write to TimescaleDB.
-			err := timescaledb.WriteToTimescaleDB(event)
-			if err != nil {
-				log.Printf("Failed to write event to TimescaleDB: %v", err)
+			// Check if the environmental variable for InfluxDB is set.
+			influxdbHost := os.Getenv("INFLUXDB_HOST")
+			if influxdbHost != "" {
+				// If set, write to InfluxDB.
+				influxdb.WriteToInfluxDB(event)
 			}
-		}
 
-		// Check if the environmental variable for Kafka is set.
-		kafkaBootstrapServers := os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
-		if kafkaBootstrapServers != "" {
-			// If set, write to Kafka.
-			err := kafka.WriteToKafka(event)
-			if err != nil {
-				log.Printf("Failed to write event to Kafka: %v", err)
+			// Check if the environmental variable for Loki is set.
+			lokiURL := os.Getenv("LOKI_URL")
+			if lokiURL != "" {
+				// If set, write to Loki.
+				loki.WriteToLoki(event)
 			}
-		}
 
-		// Check if the environmental variable for MongoDB is set.
-		mongodbURI := os.Getenv("MONGODB_URI")
-		if mongodbURI != "" {
-			// If set, write to MongoDB.
-			err := mongodb.WriteToMongoDB(event)
-			if err != nil {
-				log.Printf("Failed to write event to MongoDB: %v", err)
+			// Check if the environmental variable for TimescaleDB is set.
+			timescaleDBHost := os.Getenv("TIMESCALEDB_HOST")
+			if timescaleDBHost != "" {
+				// If set, write to TimescaleDB.
+				err := timescaledb.WriteToTimescaleDB(event)
+				if err != nil {
+					log.Printf("Failed to write event to TimescaleDB: %v", err)
+				}
+			}
+
+			// Check if the environmental variable for Kafka is set.
+			kafkaBootstrapServers := os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
+			if kafkaBootstrapServers != "" {
+				// If set, write to Kafka.
+				err := kafka.WriteToKafka(event)
+				if err != nil {
+					log.Printf("Failed to write event to Kafka: %v", err)
+				}
+			}
+
+			// Check if the environmental variable for MongoDB is set.
+			mongodbURI := os.Getenv("MONGODB_URI")
+			if mongodbURI != "" {
+				// If set, write to MongoDB.
+				err := mongodb.WriteToMongoDB(event)
+				if err != nil {
+					log.Printf("Failed to write event to MongoDB: %v", err)
+				}
 			}
 		}
 	}
 
 	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte("Event received"))
-	log.Println("Event processed successfully")
+	w.Write([]byte("Events received"))
+	log.Println("Events processed successfully")
 }
